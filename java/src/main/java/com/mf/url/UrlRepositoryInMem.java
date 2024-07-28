7c2f794c294378040e7cb62656523c306e373fae
Mohammed Fahsi <md.fahsi@gmail.com>
2024-07-28 18:51:40 +0800

    short url

--- java/src/main/java/com/mf/url/UrlRepositoryInMem.java
+++ java/src/main/java/com/mf/url/UrlRepositoryInMem.java
@@ -4,38 +4,23 @@ import net.jcip.annotations.ThreadSafe;
 
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-
 
 @ThreadSafe
 public class UrlRepositoryInMem implements UrlRepository {
-    private int capacity = 10;
-    private ConcurrentHashMap<String, String> db = new ConcurrentHashMap();
-    private ConcurrentHashMap<String, String> dbRev = new ConcurrentHashMap();
+    private ConcurrentHashMap<String, String> urlToShortUrl = new ConcurrentHashMap();
+    private ConcurrentHashMap<String, String> shortUrlToUrl = new ConcurrentHashMap();
 
-    private AtomicInteger urlCount = new AtomicInteger(100);
 
     @Override
-    public synchronized Optional<String> createIfNotExists(String url, String shortUrl) {
-        if (db.containsKey(url)) {
-            return Optional.of(db.get(url));
-        }else{
-            db.put(url,shortUrl);
-            dbRev.put(shortUrl, url);
-            urlCount.incrementAndGet();
-            return Optional.of(db.get(url));
-        }
+    public Optional<String> createIfNotExists(String url, String shortUrl) {
+        return Optional.ofNullable(urlToShortUrl.computeIfAbsent(url, k -> {
+            shortUrlToUrl.put(shortUrl, k);
+            return shortUrl;
+        }));
     }
 
     @Override
     public Optional<String> getOriginalUrl(String shortUrl) {
-        switch (dbRev.getOrDefault(shortUrl, "Nil")) {
-            case "Nil" -> {
-                return Optional.empty();
-            }
-            case String url -> {
-                return Optional.of(url);
-            }
-        }
+        return Optional.ofNullable(shortUrlToUrl.get(shortUrl));
     }
 }
